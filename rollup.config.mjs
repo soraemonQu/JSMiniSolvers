8d7597c4de5ffc88136d4645327e5bdfcbd4ff2a
Shiota Sora <shiota.sora.297@s.kyushu-u.ac.jp>
2026-01-02 21:57:02 +0900

    Create

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import terser from "@rollup/plugin-terser";
+import replace from '@rollup/plugin-replace';
 
 export default {
     input: 'src/index.js',
@@ -12,6 +13,11 @@ export default {
         sourcemap: true,
     },
     plugins: [
+        // ensure Node-specific branches are removed for browser builds
+        replace({
+            preventAssignment: true,
+            'typeof process': JSON.stringify('undefined'),
+        }),
         // use rollup-plugin-commonjs to enable "import" of emscripten code
         commonjs({
             // Emscripten includes code to require() fs and path
